87f8f735e22e3faed5826758b81cc8f8baa7c5cb
rnick1 <rnick0260@gmail.com>
2021-01-29 11:45:47 -0800

    Added pseudo code to the js as a guide.

---
+++ script.js
@@ -0,0 +1,15 @@
+// (Pseudocode)This is how I am going to utilize the information from the open weather app:
+
+// (Variables here)
+
+// button.addEventListener('click', funtion()); {
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=fdd4a04e93a26488d7033be9fdc7f5e6')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+//     .catch(err => alert('Wrong city name!'))
+// }
+
+// Note: Instead of defining the city on line 6, I can have the users define the city:
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=fdd4a04e93a26488d7033be9fdc7f5e6')
+// Notice the '+inputValue.value+' that was added instead of a specific city.
