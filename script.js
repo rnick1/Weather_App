0619c66f57df35b3859c48379fe2de3eb5a878a3
rnick1 <rnick0260@gmail.com>
2021-01-31 14:46:20 -0800

    Worked with the searchWeather function.

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-var searchBar = document.querySelector('#search-bar');
+var userSearch = document.querySelector('#search-bar');
 var searchSubmitButton = document.querySelector('#search-submit-button');
 var savedSearches = document.querySelector('#saved-searches');
 var currentWeather = document.querySelector('#current-weather');
 var fiveDayForecast = document.querySelector('#five-day-forecast');
-
+var cityName = document.querySelector('#city-name');
+var temperature = document.querySelector('#temperature');
+var humidity = document.querySelector('#humidity');
+var windSpeed = document.querySelector('#wind-speed');
+var uvIndex = document.querySelector('#uv-index');
 
 
 
 function searchWeather() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=fdd4a04e93a26488d7033be9fdc7f5e6')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+userSearch.value+'&appid=fdd4a04e93a26488d7033be9fdc7f5e6')
     .then(response => response.json())
     .then(data => console.log(data))
 
-    .catch(err => alert('Wrong city name!'))
 }
+
+button.addEventListener('click', searchWeather)
+
 
 
 
@@ -23,8 +29,8 @@ function searchWeather() {
 // (Pseudocode)This is how I am going to utilize the information from the open weather app:
 
 // (Variables here)
+// .catch(err => alert('Wrong city name!'))
 
-button.addEventListener('click', searchWeather)
 
 
 // Note: Instead of defining the city on line 6, I can have the users define the city:
