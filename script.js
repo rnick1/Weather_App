495c9d0ad5d7a703b8c033c10195d088586faba3
rnick1 <rnick0260@gmail.com>
2021-01-31 16:00:19 -0800

    Fixed the issue with the searched city not getting console logged.

--- script.js
+++ script.js
@@ -10,27 +10,16 @@ var windSpeed = document.querySelector('#wind-speed');
 var uvIndex = document.querySelector('#uv-index');
 
 
-
 function searchWeather() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+userSearch.value+'&appid=fdd4a04e93a26488d7033be9fdc7f5e6')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+userSearch.value+'&appid=fdd4a04e93a26488d7033be9fdc7f5e6';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      }
+)};
 
-}
-
-button.addEventListener('click', searchWeather)
-
-
-
-
-
-// (Pseudocode)This is how I am going to utilize the information from the open weather app:
-
-// (Variables here)
-// .catch(err => alert('Incorrect city'))
-
-
-
-// Note: Instead of defining the city on line 6, I can have the users define the city:
-//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=fdd4a04e93a26488d7033be9fdc7f5e6')
-// Notice the '+inputValue.value+' that was added instead of a specific city.
+searchSubmitButton.addEventListener('click', searchWeather)
