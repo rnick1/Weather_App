bfa7d35d6fd33ec9d66a1e5b493579c9404badb8
rnick1 <rnick0260@gmail.com>
2021-02-01 19:16:47 -0800

    Updates.

--- Notes_script.js
+++ Notes_script.js
@@ -1,18 +1,19 @@
         // Code for use with local storage:
-            var savedUserSearches = [];
-            var localStorageContent = localStorage.getItem('savedUserSearches')
-            var searches = JSON.parse(localStorageContent)
-            console.log(searches);
+        var savedUserSearches = [];
+        var localStorageContent = localStorage.getItem('savedUserSearches')
+        var searches = JSON.parse(localStorageContent)
+        console.log(searches);
 
-            
-            var savedSearchObject = {
-                city: userSearch.value,
-                link: requestWxUrl,
-            };
-            savedUserSearches.push(savedSearchObject);
-            localStorage.setItem('savedUserSearches', JSON.stringify(savedUserSearches));
+        var savedSearchObject = {
+            city: userSearch.value,
+            link: requestWxUrl,
+        };
+        savedUserSearches.push(savedSearchObject);
+        localStorage.setItem('savedUserSearches', JSON.stringify(savedUserSearches));
+        console.log(localStorageContent);
 
+        for (var i = 0; i < savedUserSearches.length; i++) {
             var savedCity = document.createElement('p');
-            savedCity.textContent = searches
-
-            savedSearches.append(savedCity)
+            savedCity = searches[i].city;
+            savedSearches.append(savedCity);
+        }
--- script.js
+++ script.js
@@ -19,18 +19,23 @@ function searchWeather() {
             var windSpeed = document.createElement('p');
             var longitude = document.createElement('p');
             var latitude = document.createElement('p');
+            var savedSearchResult = document.createElement('button');
+            // savedSearchResult.setAttribute("href" = requestUvUrl);
+
             cityName.textContent = data.name;
             temperature.textContent = 'Temperature: ' + data.main.temp + ' degrees F';
             humidity.textContent = 'Humidity: ' + data.main.humidity + ' %';
             windSpeed.textContent = 'Wind Speed: ' + data.wind.speed + ' MPH';
             longitude.textContent = 'Longitude: ' + data.coord.lon;
             latitude.textContent = 'Latitude: ' + data.coord.lat;
+            savedSearchResult.textContent = data.name;
             currentWeather.append(cityName);
             currentWeather.append(temperature);
             currentWeather.append(humidity);
             currentWeather.append(windSpeed);
             currentWeather.append(longitude);
             currentWeather.append(latitude);
+            savedSearches.append(savedSearchResult);
 
         var requestUvUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&units=imperial&appid=fdd4a04e93a26488d7033be9fdc7f5e6';
 
@@ -54,28 +59,6 @@ function searchWeather() {
             .then(function (data) {
                 console.log(data);
             })
-
-            var savedUserSearches = [];
-            var localStorageContent = localStorage.getItem('savedUserSearches')
-            var searches = JSON.parse(localStorageContent)
-            console.log(searches);
-
-            var savedSearchObject = {
-                city: userSearch.value,
-                link: requestWxUrl,
-            };
-            savedUserSearches.push(savedSearchObject);
-            localStorage.setItem('savedUserSearches', JSON.stringify(savedUserSearches));
-            console.log(localStorageContent);
-
-            for (var i = 0; i < savedUserSearches.length; i++) {
-                var savedCity = document.createElement('p');
-                savedCity = searches[i].city;
-                savedSearches.append(savedCity);
-            }
-
-
-            
 
     });
 };
