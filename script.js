ef290823a8e7430efa84524e5b7f9b262a0b4bd9
rnick1 <rnick0260@gmail.com>
2021-02-02 18:31:28 -0800

    Fixed issue with empty array not working.

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ var currentWeather = document.querySelector('#current-weather');
 var fiveDayForecast = document.querySelector('#five-day-forecast');
 var cityName = document.querySelector('#city-name');
 var searchSaveButton = document.querySelector('#search-save-button');
-var historyArray = JSON.parse(localStorage.getItem("history"))
+var historyArray = JSON.parse(localStorage.getItem("history")) || [];
 
 function searchWeather() {
     var requestWxUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + userSearch.value + '&units=imperial&appid=fdd4a04e93a26488d7033be9fdc7f5e6';
     historyArray.push(userSearch.value)
     localStorage.setItem("history", JSON.stringify(historyArray))
 
-    for(var i = 0; i < historyArray.length; i++)
-    var savedSearch = historyArray[i]
-    var displayedSearch = document.createElement('button')
-    displayedSearch.textContent = savedSearch
-    savedSearches.append(displayedSearch)
+    for(var i = 0; i < historyArray.length; i++) {
+    var button = $('<button></button')
+    button.text(historyArray[i])
+    $('.saved-searches').append(button)
+    }
 
     fetch(requestWxUrl)
         .then(function (response) {
